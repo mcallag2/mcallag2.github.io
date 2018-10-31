2e0e92e59a520ff2b65c6ae3640c6406abcf28b1
Meghan Callaghan <mcallag2@nd.edu>
2018-10-30 23:55:08 -0400

    final things

---
+++ Project-2-wireframe/Assets/behaviors.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+
+    $('#b1').click(function() {
+        $("#b1").toggleClass("invisible")
+        $("#popup1").toggleClass("visible")
+
+    });
+});
+
+
+$(document).ready(function () {
+
+    $('.x1').click(function() {
+        $("#b1").toggleClass("invisible")
+        $("#popup1").toggleClass("visible")
+
+    });
+});
+
+
+
+$(document).ready(function () {
+
+    $('#b2').click(function() {
+        $("#b2").toggleClass("invisible")
+        $("#popup2").toggleClass("visible")
+
+    });
+});
+
+
+$(document).ready(function () {
+
+    $('.x2').click(function() {
+        $("#b2").toggleClass("invisible")
+        $("#popup2").toggleClass("visible")
+
+    });
+});
+
+
+
+$(document).ready(function () {
+
+    $('#b3').click(function() {
+        $("#b3").toggleClass("invisible")
+        $("#popup3").toggleClass("visible")
+
+    });
+});
+
+
+$(document).ready(function () {
+
+    $('.x3').click(function() {
+        $("#b3").toggleClass("invisible")
+        $("#popup3").toggleClass("visible")
+
+    });
+});
+
+
+
+$(document).ready(function () {
+
+    $('#b4').click(function() {
+        $("#b4").toggleClass("invisible")
+        $("#popup4").toggleClass("visible")
+
+    });
+});
+
+
+$(document).ready(function () {
+
+    $('.x4').click(function() {
+        $("#b4").toggleClass("invisible")
+        $("#popup4").toggleClass("visible")
+
+    });
+});
